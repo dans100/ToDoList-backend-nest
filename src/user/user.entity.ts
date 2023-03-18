f2bc36eaaecc704653eca0cadfed83fb8a168b61
Daniel Sulkowski <dansulk@gmail.com>
2023-03-18 10:31:31 +0100

    Added UserItem Entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,32 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { TodosItem } from '../todos/todos-item.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    length: 55,
+  })
+  username: string;
+
+  @Column({
+    length: 60,
+  })
+  password: string;
+
+  @Column({
+    length: 256,
+  })
+  email: string;
+
+  @OneToMany(() => TodosItem, (todos: TodosItem) => todos.user)
+  todos: TodosItem[];
+}
