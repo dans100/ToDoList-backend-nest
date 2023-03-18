e84cb940428d5ef42d6a04b93c7581a1358d0d8d
Daniel Sulkowski <dansulk@gmail.com>
2023-03-18 10:31:18 +0100

    Added TodoItem Entity

---
+++ src/todos/todos-item.entity.ts
@@ -0,0 +1,39 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../user/user.entity';
+
+@Entity()
+export class TodosItem extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    length: 250,
+  })
+  description: string;
+
+  @Column({
+    length: 36,
+  })
+  user_id: string;
+
+  @Column({
+    type: 'tinyint',
+    scale: 1,
+    default: '0',
+  })
+  isCompleted: number;
+
+  @Column({
+    default: null,
+  })
+  deadline: Date;
+
+  @ManyToOne(() => User, (user: User) => user.todos)
+  user: User;
+}
