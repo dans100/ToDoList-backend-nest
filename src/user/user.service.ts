e21a0eae4dda2985447abada67c9ebed668b7108
Daniel Sulkowski <dansulk@gmail.com>
2023-03-19 14:51:34 +0100

    Added method to find user and register user with hashed password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { hash } from 'bcrypt';
+import { RegisterUserResponse } from '../interfaces/user';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async getUser(email: string): Promise<User> {
+    return await User.findOne(email);
+  }
+
+  async register(user: CreateUserDto): Promise<RegisterUserResponse> {
+    const { password, username, email } = user;
+
+    const existUser = await User.findOne({ email });
+
+    if (existUser) {
+      throw new HttpException(`User ${username} is already`, 409);
+    }
+
+    const newUser = await new User();
+
+    const hashedPassword = await hash(password, 10);
+    newUser.email = email;
+    newUser.password = hashedPassword;
+    newUser.username = username;
+
+    await newUser.save();
+
+    return {
+      message: `User ${username} has been registered`,
+    };
+  }
+}
