5fa67c79e9e05dd7df406104bf9479e651e2b054
Daniel Sulkowski <dansulk@gmail.com>
2023-03-19 14:50:19 +0100

    Added method post to user controller to register user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { RegisterUserResponse } from 'src/interfaces/user';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(@Inject(UserService) private userService: UserService) {}
+
+  @Post('/')
+  registerUser(
+    @Body() createUser: CreateUserDto,
+  ): Promise<RegisterUserResponse> {
+    return this.userService.register(createUser);
+  }
+}
