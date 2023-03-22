b72fccaece938a26b66421543fdfdf7f9c289ff6
Daniel Sulkowski <dansulk@gmail.com>
2023-03-22 16:41:28 +0100

    Added UserModule to imports

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -3,9 +3,10 @@ import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosItem } from './todos-item.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TodosItem])],
+  imports: [TypeOrmModule.forFeature([TodosItem]), UserModule],
   controllers: [TodosController],
   providers: [TodosService],
   exports: [TodosService],
