e0442fecd3d8383271ec428c09024d05cb44f70b
Daniel Sulkowski <dansulk@gmail.com>
2023-03-22 16:45:18 +0100

    Added UserService to exports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
