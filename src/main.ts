1bcc84968483c23e63d38c918410f058b8b8ecbc
Daniel Sulkowski <dansulk@gmail.com>
2023-03-22 16:30:01 +0100

    Added cookieParser, globalFilters, globalPipes, enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { GlobalExceptionFilter } from './filters/global-expection.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cookieParser());
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: 'GET,POST,DELETE,PATCH',
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
