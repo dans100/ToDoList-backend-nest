5a79d6df89c1412267536d04959a66f236ed2f94
Daniel Sulkowski <dansulk@gmail.com>
2023-03-28 14:10:35 +0200

    Added import helmet and NestExpressApplication

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { GlobalExceptionFilter } from './filters/global-expection.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  (app as NestExpressApplication).use(helmet());
   app.use(cookieParser());
-  app.useGlobalFilters(new GlobalExceptionFilter());
+  // app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
-      disableErrorMessages: true,
+      disableErrorMessages: false,
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
