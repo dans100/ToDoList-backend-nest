f29f7d590db7ede1fdf2176b4d3c21d129aa1d96
Daniel Sulkowski <dansulk@gmail.com>
2023-03-28 14:09:29 +0200

    Added validation with class-validator

--- src/dto/create-task.dto.ts
+++ src/dto/create-task.dto.ts
@@ -1,5 +1,13 @@
+import { IsDate, IsString, Length } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @Length(3, 55)
   description: string;
+
+  @IsDate()
   deadline: Date | null;
+
+  @IsString()
   userId: string;
 }
--- src/dto/create-user.dto.ts
+++ src/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(7, 60)
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 55)
   username: string;
 }
