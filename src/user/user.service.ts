7de9692c296898706c9ce72a1bef79f59b9e2280
Daniel Sulkowski <dansulk@gmail.com>
2023-03-28 14:12:16 +0200

    Added import MailModule and MailService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,27 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { hash } from 'bcrypt';
 import { RegisterUserResponse } from '../interfaces/user';
+import { MailService } from '../mail/mail.service';
+import { registerUserInfoEmailTemplate } from '../templates/mail/register-user-info';
 
 @Injectable()
 export class UserService {
+  constructor(@Inject(MailService) private mailService: MailService) {}
+
   async getUser(email: string): Promise<User> {
-    return await User.findOne(email);
+    const user = await User.findOne(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   async register(user: CreateUserDto): Promise<RegisterUserResponse> {
@@ -17,19 +31,24 @@ export class UserService {
 
     if (existUser) {
       throw new HttpException(`User ${username} is already`, 409);
-    } else {
-      const newUser = await new User();
-
-      const hashedPassword = await hash(password, 10);
-      newUser.email = email;
-      newUser.password = hashedPassword;
-      newUser.username = username;
-
-      await newUser.save();
-
-      return {
-        message: `User ${username} has been registered`,
-      };
     }
+
+    const newUser = await new User();
+
+    const hashedPassword = await hash(password, 10);
+    newUser.email = email;
+    newUser.password = hashedPassword;
+    newUser.username = username;
+
+    await newUser.save();
+    await this.mailService.sendMail(
+      email,
+      'Registration Confirmation',
+      registerUserInfoEmailTemplate(username),
+    );
+
+    return {
+      message: `User ${username} has been registered`,
+    };
   }
 }
