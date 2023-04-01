7c745aa267c7fedb52b55bbe8292b460712da689
Daniel Sulkowski <dansulk@gmail.com>
2023-04-01 11:01:56 +0200

    Added method GET, POST, DELETE to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthLoginDto } from './dto/auth-login.dto';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '../user/user.entity';
+import { UserObj } from '../decorators/user-obj.decorator';
+import { Response } from 'express';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('/')
+export class AuthController {
+  constructor(@Inject(AuthService) private authService: AuthService) {}
+
+  @Get('/login')
+  @UseGuards(AuthGuard('jwt'))
+  async login(@Res() res: Response) {
+    return res.json({ ok: true });
+  }
+
+  @Post('/login')
+  async phoneRegister(
+    @Body() req: AuthLoginDto,
+    @Res() res: Response,
+  ): Promise<any> {
+    return this.authService.login(req, res);
+  }
+
+  @Delete('/logout')
+  @UseGuards(AuthGuard('jwt'))
+  async logout(@UserObj() user: User, @Res() res: Response) {
+    return this.authService.logout(user, res);
+  }
+}
